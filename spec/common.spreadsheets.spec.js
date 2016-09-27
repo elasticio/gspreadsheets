bf70fb73b77f192a0daceb7d482837197b2fbf50
Renat Zubairov <renat.zubairov@gmail.com>
2016-09-27 11:22:39 +0200

    Tested commons

--- spec/common.spreadsheets.spec.js
+++ spec/common.spreadsheets.spec.js
@@ -1,4 +1,4 @@
-describe('Common function listSpreadsheets', function () {
+describe('Common function', function () {
     var nock = require('nock'), cfg, cb;
     var common;
     var that;
@@ -15,110 +15,195 @@ describe('Common function listSpreadsheets', function () {
         common = require('../lib/common.js');
     });
 
-    it('Should provide message if no credentials is set', function () {
+    describe('listSpreadsheets', function() {
 
-        common.listSpreadsheets.call(that, {}, cb);
+        it('Should provide message if no credentials is set', function () {
 
-        waitsFor(function () {
-            return cb.callCount;
+            common.listSpreadsheets.call(that, {}, cb);
+
+            waitsFor(function () {
+                return cb.callCount;
+            });
+
+            runs(function () {
+                expect(cb).toHaveBeenCalled();
+                expect(cb.calls.length).toEqual(1);
+                expect(cb.calls[0].args[0]).toEqual('Missing oauth access token');
+            });
         });
 
-        runs(function () {
-            expect(cb).toHaveBeenCalled();
-            expect(cb.calls.length).toEqual(1);
-            expect(cb.calls[0].args[0]).toEqual('Missing oauth access token');
+        it('should react on auth errors when refreshing token', function () {
+
+            // Refresh token
+            nock('https://accounts.google.com/').post('/o/oauth2/token', {
+                grant_type: 'refresh_token',
+                client_id: 'app-id',
+                client_secret: 'app-secret',
+                refresh_token: 'some-refresh-token'
+            }).reply(500, "Server error");
+
+            common.listSpreadsheets.call(that, cfg, cb);
+
+            waitsFor(function () {
+                return cb.callCount;
+            });
+
+            runs(function () {
+                expect(cb).toHaveBeenCalled();
+                expect(cb.calls.length).toEqual(1);
+                expect(cb.calls[0].args[0].message).toEqual('Server error');
+                expect(cb.calls[0].args[0]).toEqual({code: 500});
+            });
         });
+
+        it('should react on errors when fetching resource', function () {
+
+            // Refresh token
+            nock('https://accounts.google.com/').post('/o/oauth2/token', {
+                grant_type: 'refresh_token',
+                client_id: 'app-id',
+                client_secret: 'app-secret',
+                refresh_token: 'some-refresh-token'
+            }).reply(200, {
+                access_token: 'access-token-2',
+                refresh_token: 'refresh-token-2'
+            });
+
+            // List all spreadsheets
+            nock('https://www.googleapis.com')
+                .get('/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&pageSize=1000')
+                .reply(500, 'Server error');
+
+            common.listSpreadsheets.call(that, cfg, cb);
+
+            waitsFor(function () {
+                return cb.callCount;
+            });
+
+            runs(function () {
+                expect(cb).toHaveBeenCalled();
+                expect(cb.calls.length).toEqual(1);
+                expect(cb.calls[0].args[0].message).toEqual('Server error');
+                expect(cb.calls[0].args[0].code).toEqual(500);
+            });
+        });
+
+        it('should list spreadsheets', function () {
+
+            // Refresh token
+            nock('https://accounts.google.com/').post('/o/oauth2/token', {
+                grant_type: 'refresh_token',
+                client_id: 'app-id',
+                client_secret: 'app-secret',
+                refresh_token: 'some-refresh-token'
+            }).reply(200, {
+                access_token: 'access-token-2',
+                refresh_token: 'refresh-token-2'
+            });
+
+            // List all spreadsheets
+            nock('https://www.googleapis.com')
+                .get('/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&pageSize=1000')
+                .replyWithFile(200, __dirname + '/data/response.json');
+
+            common.listSpreadsheets.call(that, cfg, cb);
+
+            waitsFor(function () {
+                return cb.callCount;
+            });
+
+            runs(function () {
+                expect(cb).toHaveBeenCalled();
+                expect(cb.calls.length).toEqual(1);
+                expect(cb.calls[0].args[0]).toEqual(null);
+                var result = cb.calls[0].args[1];
+                // Four spreadsheets
+                expect(Object.keys(result).length).toEqual(2);
+                expect(Object.keys(result)[0]).toEqual("one");
+                expect(result[Object.keys(result)[0]]).toEqual("One");
+            });
+        });
+
     });
 
-    it('should react on auth errors when refreshing token', function () {
+    describe('getMetaModel', function() {
 
-        // Refresh token
-        nock('https://accounts.google.com/').post('/o/oauth2/token', {
-            grant_type: 'refresh_token',
-            client_id: 'app-id',
-            client_secret: 'app-secret',
-            refresh_token: 'some-refresh-token'
-        }).reply(500, "Server error");
-
-        common.listSpreadsheets.call(that, cfg, cb);
-
-        waitsFor(function () {
-            return cb.callCount;
+        beforeEach(function () {
+            cfg = {
+                spreadsheetId: 'spreadsheetID',
+                oauth: {refresh_token: 'some-refresh-token', access_token: 'some-access-id'}
+            };
         });
 
-        runs(function () {
-            expect(cb).toHaveBeenCalled();
-            expect(cb.calls.length).toEqual(1);
-            expect(cb.calls[0].args[0].message).toEqual('Server error');
-            expect(cb.calls[0].args[0]).toEqual({code: 500});
+        it('should react on errors when fetching resource', function () {
+
+            // Refresh token
+            nock('https://accounts.google.com/').post('/o/oauth2/token', {
+                grant_type: 'refresh_token',
+                client_id: 'app-id',
+                client_secret: 'app-secret',
+                refresh_token: 'some-refresh-token'
+            }).reply(200, {
+                access_token: 'access-token-2',
+                refresh_token: 'refresh-token-2'
+            });
+
+            // Get first rows
+            nock('https://sheets.googleapis.com')
+                .get('/v4/spreadsheets/spreadsheetID/values/1:3?majorDimension=ROWS&dateTimeRenderOption=FORMATTED_STRING')
+                .reply(500, 'Server error');
+
+            common.getMetaModel.call(that, cfg, cb);
+
+            waitsFor(function () {
+                return cb.callCount;
+            });
+
+            runs(function () {
+                expect(cb).toHaveBeenCalled();
+                expect(cb.calls.length).toEqual(1);
+                expect(cb.calls[0].args[0].message).toEqual('Server error');
+                expect(cb.calls[0].args[0].code).toEqual(500);
+            });
         });
+
+        it('should return metamodel', function () {
+
+            // Refresh token
+            nock('https://accounts.google.com/').post('/o/oauth2/token', {
+                grant_type: 'refresh_token',
+                client_id: 'app-id',
+                client_secret: 'app-secret',
+                refresh_token: 'some-refresh-token'
+            }).reply(200, {
+                access_token: 'access-token-2',
+                refresh_token: 'refresh-token-2'
+            });
+
+            // Get first rows
+            nock('https://sheets.googleapis.com')
+                .get('/v4/spreadsheets/spreadsheetID/values/1:3?majorDimension=ROWS&dateTimeRenderOption=FORMATTED_STRING')
+                .replyWithFile(200, __dirname + '/data/rows.json');
+
+            common.getMetaModel.call(that, cfg, cb);
+
+            waitsFor(function () {
+                return cb.callCount;
+            });
+
+            runs(function () {
+                expect(cb).toHaveBeenCalled();
+                expect(cb.calls.length).toEqual(1);
+                expect(cb.calls[0].args[0]).toEqual(null);
+                var result = cb.calls[0].args[1];
+                expect(Object.keys(result).length).toEqual(2);
+                expect(Object.keys(result.in)).toEqual([]);
+                expect(Object.keys(result.out)).toEqual([ 'type', 'properties' ]);
+                expect(result.out.type).toEqual('object');
+                expect(Object.keys(result.out.properties)).toEqual([ 'rowIndex', 'col0', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9', 'col10', 'col11', 'col12' ]);
+            });
+        });
+
     });
 
-    it('should react on errors when fetching resource', function () {
-
-        // Refresh token
-        nock('https://accounts.google.com/').post('/o/oauth2/token', {
-            grant_type: 'refresh_token',
-            client_id: 'app-id',
-            client_secret: 'app-secret',
-            refresh_token: 'some-refresh-token'
-        }).reply(200, {
-            access_token: 'access-token-2',
-            refresh_token: 'refresh-token-2'
-        });
-
-        // List all spreadsheets
-        nock('https://www.googleapis.com')
-            .get('/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&pageSize=1000')
-            .reply(500, 'Server error');
-
-        common.listSpreadsheets.call(that, cfg, cb);
-
-        waitsFor(function () {
-            return cb.callCount;
-        });
-
-        runs(function () {
-            expect(cb).toHaveBeenCalled();
-            expect(cb.calls.length).toEqual(1);
-            expect(cb.calls[0].args[0].message).toEqual('Server error');
-            expect(cb.calls[0].args[0].code).toEqual(500);
-        });
-    });
-
-    it('should list spreadsheets', function () {
-
-        // Refresh token
-        nock('https://accounts.google.com/').post('/o/oauth2/token', {
-            grant_type: 'refresh_token',
-            client_id: 'app-id',
-            client_secret: 'app-secret',
-            refresh_token: 'some-refresh-token'
-        }).reply(200, {
-            access_token: 'access-token-2',
-            refresh_token: 'refresh-token-2'
-        });
-
-        // List all spreadsheets
-        nock('https://www.googleapis.com')
-            .get('/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&pageSize=1000')
-            .replyWithFile(200, __dirname + '/data/response.json');
-
-        common.listSpreadsheets.call(that, cfg, cb);
-
-        waitsFor(function () {
-            return cb.callCount;
-        });
-
-        runs(function () {
-            expect(cb).toHaveBeenCalled();
-            expect(cb.calls.length).toEqual(1);
-            expect(cb.calls[0].args[0]).toEqual(null);
-            var result = cb.calls[0].args[1];
-            // Four spreadsheets
-            expect(Object.keys(result).length).toEqual(2);
-            expect(Object.keys(result)[0]).toEqual("one");
-            expect(result[Object.keys(result)[0]]).toEqual("One");
-        });
-    });
 });
