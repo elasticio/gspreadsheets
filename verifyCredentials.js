1eb880b4b15a3c4e0f6f83a65d482accaeb90cc9
ElBabbler <gladikov.denis@gmail.com>
2019-06-24 10:20:14 +0300

    Removing platform logger usage on credentials verification.

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,4 +1,3 @@
-const logger = require('@elastic.io/component-logger')();
 const { GoogleOauth2Client } = require('./lib/client');
 
 
@@ -6,9 +5,9 @@ async function verify(credentials) {
   const googleOauth2Client = new GoogleOauth2Client(credentials);
   try {
     googleOauth2Client.listOfSpreadsheets();
-    logger.info('Credentials was successfully verified');
+    console.log('Credentials was successfully verified');
   } catch (e) {
-    logger.error('Error while request to Google API: o%', e);
+    console.log('Error while request to Google API: o%', e);
     throw e;
   }
 }
