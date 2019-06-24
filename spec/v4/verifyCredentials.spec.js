5842ac49e8793b582179b33f3683ca7c6bdc5f55
ElBabbler <gladikov.denis@gmail.com>
2019-06-24 12:32:07 +0300

    Fixing credentials verification

---
+++ spec/v4/verifyCredentials.spec.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const nock = require('nock');
+const sinon = require('sinon');
+
+const verify = require('../../verifyCredentials');
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe('Verify Credentials', function () {
+  this.timeout(5000);
+  let emitter;
+
+  let configuration;
+  before(() => {
+    if (fs.existsSync('.env')) {
+      // eslint-disable-next-line global-require
+      require('dotenv').config();
+    }
+    configuration = {
+      oauth: {
+        access_token: 'some_token',
+        expiry_date: 5000000000000,
+        refresh_token: 'some_refresh_token',
+        scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets',
+        token_type: 'Bearer',
+      },
+    };
+  });
+
+  beforeEach(() => {
+    emitter = {
+      emit: sinon.spy(),
+      logger: {
+        trace: sinon.spy(),
+        debug: sinon.spy(),
+      }
+    };
+  });
+
+  it(' successful update on mocked list Of Spreadsheets', async () => {
+
+    nock('https://www.googleapis.com')
+      .get(
+        '/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&fields=nextPageToken%2C%20files%28id%2C%20name%29',
+      )
+      .reply(200, {
+        files: [
+          {
+            id: 1,
+            name: 'Sheet1',
+          },
+          {
+            id: 2,
+            name: 'Sheet2',
+          },
+        ],
+      });
+
+    await verify(configuration);
+  });
+});
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,13 +1,14 @@
 const { GoogleOauth2Client } = require('./lib/client');
+const logger = require('@elastic.io/component-logger')();
 
 
 async function verify(credentials) {
-  const googleOauth2Client = new GoogleOauth2Client(credentials);
+  const googleOauth2Client = new GoogleOauth2Client(credentials, { logger });
   try {
-    googleOauth2Client.listOfSpreadsheets();
+    await googleOauth2Client.listOfSpreadsheets();
     console.log('Credentials was successfully verified');
   } catch (e) {
-    console.log('Error while request to Google API: o%', e);
+    console.log('Error while request to Google API: %o', e);
     throw e;
   }
 }
