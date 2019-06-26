c8f0f4709b845e88862ab67feb333a47b9e15e73
ShkarupaNick <shkarupanick@gmail.com>
2019-06-26 15:45:35 +0300

    fixed range bug

--- spec/v4/triggers/newSpreadsheedRow.spec.js
+++ spec/v4/triggers/newSpreadsheedRow.spec.js
@@ -12,7 +12,7 @@ const { process: processTrigger } = require(
 
 process.env.GOOGLE_APP_ID = '708580332494-tbo7u3g58ucof7sni4kklmia2v8thtks.apps.googleusercontent.com';
 process.env.GOOGLE_APP_SECRET = 'JGmWX6XH5p0-0niRV10m8DLb';
-process.env.ACCESS_TOKEN = 'ya29.GlssByV2tMnuAu6EHoAPwiStnXpXDFSOvsPoDzL0h7QNoyI5uMRLWqFZ-tulVtRkUpkkXGtA_lN_LQPVBBebgfcqL34vE4Rqmkq-IDo3gVtscpuzy0SSZ6tMQl8y';
+process.env.ACCESS_TOKEN = 'ya29.GlwzB0Owy9YKIifRKK9TLOIi6EbcKgGZj7A9L52XfqoAbUGOSIroU-fBaym_TTWD0J50a1mMkQZlrxTvf-wNS0ugbVj03jtWtlX1SP2RCupsXRR2nzJ4uvf_V2ueHw';
 process.env.REFRESH_TOKEN = '1/-TrS0eh4zSVWaUJ27JDmxog008EDw-s9CrgCTUCvrOQ';
 process.env.TEST_REDIRECT_URL = 'urn:ietf:wg:oauth:2.0:oob';
 
@@ -32,7 +32,7 @@ describe('newSpreadsheetRow', () => {
         expiry_date: new Date().getTime() + 10000000000,
       },
       spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
-      worksheetName: 'Sheet4',
+      worksheetName: 'ROWS Dimension',
       includeHeader: 'yes',
       dimension: 'ROWS',
     };
@@ -63,24 +63,19 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: ['Sheet4!A1:ZZZ1', 'Sheet4!A2:ZZZ1001'],
+              ranges: ['ROWS Dimension!A1:GJH1', 'ROWS Dimension!A2:GJH1001'],
               majorDimension: 'ROWS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet4!A1:AA1',
+                  range: '\'ROWS Dimension\'!A1:AA1',
                   majorDimension: 'ROWS',
-                  values: [
-                    [
-                      'FirstName',
-                      'LastName',
-                    ],
-                  ],
+                  values: [['FirstName', 'LastName']],
                 },
                 {
-                  range: 'Sheet4!A2:AA942',
+                  range: '\'ROWS Dimension\'!A2:AA942',
                   majorDimension: 'ROWS',
                   values: [
                     ['Tom1', 'Smith1'],
@@ -92,20 +87,15 @@ describe('newSpreadsheetRow', () => {
                     ['Tom7', 'Smith7'],
                     ['Tom8', 'Smith8'],
                     ['Tom9', 'Smith9'],
-                    ['Tom10', 'Smith10'],
-                  ],
-                },
-              ],
+                    ['Tom10', 'Smith10']],
+                }],
             },
             ['Content-Type', 'application/json; charset=UTF-8']);
           // nock.recorder.rec();
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(11);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
         });
 
       it('lastEmittedLine: 4, includeHeader: yes',
@@ -127,19 +117,19 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: ['Sheet4!A1:ZZZ1', 'Sheet4!A5:ZZZ1004'],
+              ranges: ['ROWS Dimension!A1:GJH1', 'ROWS Dimension!A5:GJH1004'],
               majorDimension: 'ROWS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet4!1:1',
+                  range: '\'ROWS Dimension\'!A1:AA1',
                   majorDimension: 'ROWS',
                   values: [['FirstName', 'LastName']],
                 },
                 {
-                  range: 'Sheet4!A5:AA942',
+                  range: '\'ROWS Dimension\'!A5:AA942',
                   majorDimension: 'ROWS',
                   values: [
                     ['Tom4', 'Smith4'],
@@ -156,10 +146,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(8);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             FirstName: 'Tom4',
             LastName: 'Smith4',
@@ -186,14 +173,14 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: 'Sheet4!A1:ZZZ1000',
+              ranges: 'ROWS Dimension!A1:GJH1000',
               majorDimension: 'ROWS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet4!A1:AA942',
+                  range: '\'ROWS Dimension\'!A1:AA942',
                   majorDimension: 'ROWS',
                   values: [
                     ['FirstName', 'LastName'],
@@ -215,10 +202,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(12);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             A: 'FirstName',
             B: 'LastName',
@@ -245,14 +229,14 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: 'Sheet4!A5:ZZZ1004',
+              ranges: 'ROWS Dimension!A5:GJH1004',
               majorDimension: 'ROWS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet4!A5:AA942',
+                  range: '\'ROWS Dimension\'!A5:AA942',
                   majorDimension: 'ROWS',
                   values: [
                     ['Tom4', 'Smith4'],
@@ -270,10 +254,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(8);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             A: 'Tom4',
             B: 'Smith4',
@@ -287,7 +268,7 @@ describe('newSpreadsheetRow', () => {
           snapshot = { lastEmittedLine: 0 };
           configuration.includeHeader = 'yes';
           configuration.dimension = 'COLUMNS';
-          configuration.worksheetName = 'Sheet1';
+          configuration.worksheetName = 'COLUMNS Dimension';
 
           nock('https://www.googleapis.com:443', { encodedQueryParams: true })
             .get('/drive/v3/files/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM')
@@ -304,19 +285,19 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: ['Sheet1!A1:A18278', 'Sheet1!B1:ALM18278'],
+              ranges: ['COLUMNS Dimension!A1:A5000', 'COLUMNS Dimension!B1:ALM5000'],
               majorDimension: 'COLUMNS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet1!A1:A982',
+                  range: '\'COLUMNS Dimension\'!A1:A982',
                   majorDimension: 'COLUMNS',
                   values: [['FirstName', 'LastName']],
                 },
                 {
-                  range: 'Sheet1!B1:Z982',
+                  range: '\'COLUMNS Dimension\'!B1:AB982',
                   majorDimension: 'COLUMNS',
                   values: [
                     ['Tom1', 'Smith1'],
@@ -337,10 +318,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(11);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             FirstName: 'Tom1',
             LastName: 'Smith1',
@@ -352,7 +330,7 @@ describe('newSpreadsheetRow', () => {
           snapshot = { lastEmittedLine: 4 };
           configuration.includeHeader = 'yes';
           configuration.dimension = 'COLUMNS';
-          configuration.worksheetName = 'Sheet1';
+          configuration.worksheetName = 'COLUMNS Dimension';
 
           nock('https://www.googleapis.com:443', { encodedQueryParams: true })
             .get('/drive/v3/files/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM')
@@ -369,19 +347,19 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: ['Sheet1!A1:A18278', 'Sheet1!E1:ALP18278'],
+              ranges: ['COLUMNS Dimension!A1:A5000', 'COLUMNS Dimension!E1:ALP5000'],
               majorDimension: 'COLUMNS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet1!A1:A982',
+                  range: '\'COLUMNS Dimension\'!A1:A982',
                   majorDimension: 'COLUMNS',
                   values: [['FirstName', 'LastName']],
                 },
                 {
-                  range: 'Sheet1!E1:Z982',
+                  range: '\'COLUMNS Dimension\'!E1:AB982',
                   majorDimension: 'COLUMNS',
                   values: [
                     ['Tom4', 'Smith4'],
@@ -399,10 +377,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(8);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             FirstName: 'Tom4',
             LastName: 'Smith4',
@@ -414,7 +389,7 @@ describe('newSpreadsheetRow', () => {
           snapshot = { lastEmittedLine: 0 };
           configuration.includeHeader = 'no';
           configuration.dimension = 'COLUMNS';
-          configuration.worksheetName = 'Sheet1';
+          configuration.worksheetName = 'COLUMNS Dimension';
 
           nock('https://www.googleapis.com:443', { encodedQueryParams: true })
             .get('/drive/v3/files/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM')
@@ -431,7 +406,7 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: 'Sheet1!A1:ALL18278',
+              ranges: 'COLUMNS Dimension!A1:ALL5000',
               majorDimension: 'COLUMNS',
             })
             .reply(200, {
@@ -460,10 +435,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(12);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             1: 'FirstName',
             2: 'LastName',
@@ -475,7 +447,7 @@ describe('newSpreadsheetRow', () => {
           snapshot = { lastEmittedLine: 4 };
           configuration.includeHeader = 'no';
           configuration.dimension = 'COLUMNS';
-          configuration.worksheetName = 'Sheet1';
+          configuration.worksheetName = 'COLUMNS Dimension';
 
           nock('https://www.googleapis.com:443', { encodedQueryParams: true })
             .get('/drive/v3/files/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM')
@@ -492,14 +464,14 @@ describe('newSpreadsheetRow', () => {
               '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
             )
             .query({
-              ranges: 'Sheet1!E1:ALP18278',
+              ranges: 'COLUMNS Dimension!E1:ALP5000',
               majorDimension: 'COLUMNS',
             })
             .reply(200, {
               spreadsheetId: '1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM',
               valueRanges: [
                 {
-                  range: 'Sheet1!E1:Z982',
+                  range: '\'COLUMNS Dimension\'!E1:AB982',
                   majorDimension: 'COLUMNS',
                   values: [
                     ['Tom4', 'Smith4'],
@@ -517,10 +489,7 @@ describe('newSpreadsheetRow', () => {
           await processTrigger.call(context, {}, configuration, snapshot);
           expect(context.emit.getCalls().length).to.be.equal(8);
           expect(context.emit.lastCall.args[0]).to.be.equal('snapshot');
-          expect(context.emit.lastCall.args[1]).to.deep.equal({
-            modifiedTime: 1561025714763,
-            lastEmittedLine: 11,
-          });
+          expect(context.emit.lastCall.args[1].lastEmittedLine).to.be.equal(11);
           expect(context.emit.firstCall.args[1].body).to.deep.equal({
             1: 'Tom4',
             2: 'Smith4',
@@ -576,7 +545,7 @@ describe('newSpreadsheetRow', () => {
             '/v4/spreadsheets/1gzn1CA_lvkzrjWETWhUoh0cyY_GBvgwK55IAhfGGVlM/values:batchGet',
           )
           .query({
-            ranges: 'Sheet1!L1:ALW18278',
+            ranges: 'Sheet1!L1:ALW5000',
             majorDimension: 'COLUMNS',
           })
           .reply(200, {
