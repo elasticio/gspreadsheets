570724630e02d82a60731861c2640e772ce8076c
ElBabbler <gladikov.denis@gmail.com>
2019-06-26 10:31:04 +0300

    Removing eio platform logger with debug lib.

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,14 +1,13 @@
 const { GoogleOauth2Client } = require('./lib/client');
-const logger = require('@elastic.io/component-logger')();
-
+const debug = require('debug')('Credentials');
 
 async function verify(credentials) {
-  const googleOauth2Client = new GoogleOauth2Client(credentials, { logger });
+  const googleOauth2Client = new GoogleOauth2Client(credentials, this);
   try {
     await googleOauth2Client.listOfSpreadsheets();
-    console.log('Credentials was successfully verified');
+    debug('Credentials was successfully verified');
   } catch (e) {
-    console.log('Error while request to Google API: %o', e);
+    debug('Error while request to Google API: %o', e);
     throw e;
   }
 }
