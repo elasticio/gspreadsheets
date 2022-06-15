7d05442babf0c8f0ee6524b11fe917861f23a668
shulkaolka <olga.shukel@gmail.com>
2022-06-15 17:22:40 +0300

    fix verify credentials

--- spec/verifyCredentials.spec.js
+++ spec/verifyCredentials.spec.js
@@ -1,49 +1,42 @@
-const fs = require('fs');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-const nock = require('nock');
+const logger = require('@elastic.io/component-logger')();
+// const nock = require('nock');
 
+const sinon = require('sinon');
 const verify = require('../verifyCredentials');
 
 chai.use(chaiAsPromised);
+let context;
 
 describe('Verify Credentials', () => {
-  let configuration;
-  before(() => {
-    if (fs.existsSync('.env')) {
-      // eslint-disable-next-line global-require
-      require('dotenv').config();
-    }
-    configuration = {
-      oauth: {
-        access_token: 'some_token',
-        expiry_date: 5000000000000,
-        refresh_token: 'some_refresh_token',
-        scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets',
-        token_type: 'Bearer',
-      },
-    };
+  const configuration = {
+    secretId: '62a98dcac5becb0011e5c0af',
+  };
+  beforeEach(() => {
+    context = { logger, emit: sinon.spy() };
   });
 
 
   it(' successful update on mocked list Of Spreadsheets', async () => {
-    nock('https://www.googleapis.com')
-      .get(
-        '/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&fields=nextPageToken%2C%20files%28id%2C%20name%29',
-      )
-      .reply(200, {
-        files: [
-          {
-            id: 1,
-            name: 'Sheet1',
-          },
-          {
-            id: 2,
-            name: 'Sheet2',
-          },
-        ],
-      });
+    // nock('https://www.googleapis.com')
+    //   .get(
+    // eslint-disable-next-line max-len
+    //     '/drive/v3/files?q=mimeType%3D%27application%2Fvnd.google-apps.spreadsheet%27&fields=nextPageToken%2C%20files%28id%2C%20name%29',
+    //   )
+    //   .reply(200, {
+    //     files: [
+    //       {
+    //         id: 1,
+    //         name: 'Sheet1',
+    //       },
+    //       {
+    //         id: 2,
+    //         name: 'Sheet2',
+    //       },
+    //     ],
+    //   });
 
-    await verify(configuration);
+    await verify.call(context, configuration);
   });
 });
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,36 +1,15 @@
-const logger = require('@elastic.io/component-logger')();
 const { GoogleOauth2Client } = require('./lib/client');
 
 async function verify(credentials) {
-  // for now sailor hasn't opportunity emit something from verify credentials context
-  const context = {
-    logger,
-    emit: (emitType) => {
-      logger.warn(`Can not call ${emitType} from verify credentials context.`);
-    },
-  };
+  this.logger.info('Start to verify credentials, config: %j', credentials);
 
-  // eslint-disable-next-line no-use-before-define
-  checkOauth2EnvarsPresence();
-
-  const googleOauth2Client = new GoogleOauth2Client(credentials, context);
+  const googleOauth2Client = new GoogleOauth2Client(credentials, this);
   try {
-    await googleOauth2Client.listOfSpreadsheets();
-    logger.info('Credentials was successfully verified');
+    const result = await googleOauth2Client.listOfSpreadsheets();
+    this.logger.info('Credentials was successfully verified, result %j', result);
   } catch (e) {
-    logger.error('Credentials verification failed');
+    this.logger.error('Credentials verification failed');
     throw e;
-  }
-}
-
-function checkOauth2EnvarsPresence() {
-  if (!process.env.OAUTH_CLIENT_ID) {
-    if (!process.env.OAUTH_CLIENT_SECRET) {
-      throw new Error('Environment variables are missed: OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET');
-    }
-    throw new Error('Environment variables are missed: OAUTH_CLIENT_ID');
-  } else if (!process.env.OAUTH_CLIENT_SECRET) {
-    throw new Error('Environment variables are missed: OAUTH_CLIENT_SECRET');
   }
 }
 
